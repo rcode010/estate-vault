456b407bbc827edf58b4d85300babee6a0813404
Raman <ramanazad881@gmail.com>
2026-08-28 23:49:21 +0300

    fix: convert contracts from nested repo to standard folder

---
+++ contracts/src/EstateVault.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract EstateVault {
+    address public admin;
+    uint256 public totalSupply;
+    uint256 public tokensSold;
+    uint256 public pricePerToken;
+    string public propertyMetadataURI;
+
+    mapping(address => uint256) public balances;
+}
